1eb37613ab1fbeaa1e9342d1ed7026f382d547fb
lexcf <77665926+lexcf@users.noreply.github.com>
2022-07-26 19:20:11 +0300

    Добавьте файлы проекта.

---
+++ DeleteUsers/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace DeleteUsers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usersListBox = new System.Windows.Forms.ListBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usersListBox
+            // 
+            this.usersListBox.FormattingEnabled = true;
+            this.usersListBox.ItemHeight = 16;
+            this.usersListBox.Items.AddRange(new object[] {
+            ""});
+            this.usersListBox.Location = new System.Drawing.Point(16, 15);
+            this.usersListBox.Margin = new System.Windows.Forms.Padding(4);
+            this.usersListBox.Name = "usersListBox";
+            this.usersListBox.Size = new System.Drawing.Size(573, 420);
+            this.usersListBox.TabIndex = 1;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(599, 57);
+            this.updateButton.Margin = new System.Windows.Forms.Padding(4);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(172, 43);
+            this.updateButton.TabIndex = 2;
+            this.updateButton.Text = "Обновить";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(599, 119);
+            this.deleteButton.Margin = new System.Windows.Forms.Padding(4);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(172, 43);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.Text = "Удалить";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 455);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.usersListBox);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Удаление пользователей";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox usersListBox;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ DeleteUsers/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Security.AccessControl;
+using System.Management;
+using System.Security.Permissions;
+
+namespace DeleteUsers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> users = new List<string>();
+        string selectedUser;
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            usersListBox.Items.Clear();
+            string[] allfiles = Directory.GetDirectories("C:\\Users");
+            foreach (string filename in allfiles)
+            {
+                if(!(filename.Substring(9).Equals("All Users") || filename.Substring(9).Equals("Default") || filename.Substring(9).Equals("Default User") || filename.Substring(9).Equals("Public")))
+                {
+                    usersListBox.Items.Add(filename.Substring(9));
+                    users.Add(filename.Substring(9));
+                }
+                    
+            }
+        }
+
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(usersListBox.SelectedItems.Count == 0) { MessageBox.Show("Выберите пользователя!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+                selectedUser = usersListBox.SelectedItem.ToString();
+                var response = MessageBox.Show("Удалить пользователя " + selectedUser + "?", "Удаление пользователя", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (response == DialogResult.Yes)
+                {
+                    string[] cmdArr = { "net user ", selectedUser, " /delete" };
+                    string path = string.Concat("C:\\Users\\", selectedUser);
+                    System.Diagnostics.Process.Start("cmd.exe", "/C " + string.Concat(cmdArr));
+
+                    FileIOPermission f2 = new FileIOPermission(FileIOPermissionAccess.Read | FileIOPermissionAccess.Write, path);
+                    try
+                    {
+                        f2.Demand();
+                    }
+                    catch (Exception s)
+                    {
+                        MessageBox.Show(s.Message);
+                    }
+
+
+                    var di = new DirectoryInfo(path);
+                    //di.Attributes |= FileAttributes.Normal;
+                    del(di);
+
+                    //System.Diagnostics.Process.Start("cmd.exe", "/C rm -r " + path);
+                    Directory.Delete(path, true);
+
+                }
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void del(DirectoryInfo dir)
+        {
+            
+            foreach (var file in dir.GetFiles())
+            {
+                file.Attributes = FileAttributes.Normal;
+                //File.Delete(file.FullName);
+            }
+
+            if (dir.GetDirectories().Length == 0) return;
+            foreach (var subDir in dir.GetDirectories())
+            {
+                if (subDir.Attributes.HasFlag(FileAttributes.ReparsePoint))
+                {
+                    subDir.Attributes = FileAttributes.Normal;
+                    subDir.Delete(true);
+                    
+                }
+                else
+                {
+                    //subDir.Attributes = FileAttributes.Directory;
+                    del(subDir);
+                    subDir.Attributes = FileAttributes.Normal;
+                    subDir.Delete(true);
+                    //subDir.Attributes = FileAttributes.Normal;
+                }
+                
+            }
+            
+        }
+
+    }
+}
